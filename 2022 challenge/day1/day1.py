885755ccce51ca62d303fb39caceb04bbf57e837
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-05-03 06:29:45 +0800

    finished day1

--- 2022 challenge/day1/day1.py
+++ 2022 challenge/day1/day1.py
@@ -1,5 +1,18 @@
-with open('example.txt') as file: puzzle = [i.strip() for i in file.readlines()]
+with open('2022 challenge/day1/input.txt') as file: puzzle = [int(i.strip()) for i in file.readlines()]
 
 print(puzzle)
 
+total = 0
 
+a = []
+
+for ind, i in enumerate(puzzle):
+    if len(a) >= 60:
+        a = a[1:]
+    a.append(i)
+    avg = sum(a)/len(a)
+    if 1500 > avg or 1600 < avg:
+        if len(a) >= 60:
+            total +=1
+
+print(total)
